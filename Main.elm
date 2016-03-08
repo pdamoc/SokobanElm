1cb16406e74bb26599b868268157282b2c94ce76
Peter Damoc <pdamoc@gmail.com>
2016-03-08 15:22:21 +0200

    changed to use StartApp

--- Main.elm
+++ Main.elm
@@ -1,35 +1,42 @@
 module Main where
 
 import Game
-import Level
-import Definitions
 
 import Svg exposing (Svg)
+import StartApp
+import Effects exposing (Effects)
+
 
 import Keyboard
 import Time
 
-mailbox : Signal.Mailbox Game.Action
-mailbox = Signal.mailbox Game.ActNop
- 
 -- http://www.boxworld.matsel.de/daten/deutsch/1.htm
 
+
+effectless : (a -> b -> c) -> a -> b -> ( c, Effects d )
+effectless update action model = (update action model, Effects.none)
+
+app : StartApp.App Game.Model
+app = StartApp.start 
+  { init = (Game.initialModel, Effects.none)
+  , update = effectless Game.update
+  , view = Game.drawModel
+  , inputs = 
+    [ Signal.merge Keyboard.arrows Keyboard.wasd
+      |> Signal.map (\p -> Game.ActArrows (p.x, -p.y))
+    , Keyboard.enter
+      |> Signal.merge Keyboard.space
+      |> Time.timestamp
+      |> Signal.map (fst >> Game.ActEnter) 
+      , Keyboard.isDown 77
+       |> Signal.map (\b -> if b then Game.ActNop else Game.ActMenu)
+      , Keyboard.isDown 82
+       |> Time.timestamp 
+       |> Signal.map (\(t, b) -> if b then Game.ActNop else Game.ActRestartLevel t)
+      , Time.every 10
+       |> Signal.map Game.ActTick
+    ]
+  }
+
 main : Signal Svg
-main =
-    Signal.mergeMany [ mailbox.signal
-                     , Signal.merge Keyboard.arrows Keyboard.wasd
-                       |> Signal.map (\p -> Game.ActArrows (p.x, -p.y))
-                     , Keyboard.enter
-                       |> Signal.merge Keyboard.space
-                       |> Time.timestamp
-                       |> Signal.map (fst >> Game.ActEnter)
-                     , Keyboard.isDown 77
-                       |> Signal.map (\b -> if b then Game.ActNop else Game.ActMenu)
-                     , Keyboard.isDown 82
-                       |> Time.timestamp 
-                       |> Signal.map (\(t, b) -> if b then Game.ActNop else Game.ActRestartLevel t)
-                     , Time.every 10
-                       |> Signal.map Game.ActTick
-                     ]
-    |> Signal.foldp Game.update Game.initialModel
-    |> Signal.map (Game.drawModel mailbox.address)
+main = app.html
